2e0089f71febca1ec230729cf2986f4ad66680f5
Lachlan Sylvester <lachlan.sylvester@publicisfrontfoot.com.au>
2018-06-05 12:54:11 +1000

    add tests for uploading as file

--- test/activestorage/postgresql_test.rb
+++ test/activestorage/postgresql_test.rb
@@ -14,6 +14,39 @@ class ActiveStorage::Service::PostgreSQLServiceTest < ActiveSupport::TestCase
 
   include ActiveStorage::Service::SharedServiceTests
 
+  test "uploading file with integrity" do
+    begin
+      key  = SecureRandom.base58(24)
+      data = "Something else entirely!"
+      file = Tempfile.open("upload")
+      file.write(data)
+      file.rewind
+      @service.upload(key, file, checksum: Digest::MD5.base64digest(data))
+
+      assert_equal data, @service.download(key)
+    ensure
+      @service.delete key
+    end
+  end
+
+  test "uploading file without integrity" do
+    begin
+      key  = SecureRandom.base58(24)
+      data = "Something else entirely!"
+      file = Tempfile.open("upload")
+      file.write(data)
+      file.rewind
+
+      assert_raises(ActiveStorage::IntegrityError) do
+        @service.upload(key, file, checksum: Digest::MD5.base64digest("bad data"))
+      end
+
+      assert_not @service.exist?(key)
+    ensure
+      @service.delete key
+    end
+  end
+
   test "url generation" do
     assert_match(/^https:\/\/example.com\/rails\/active_storage\/disk\/.*\/avatar\.png\?content_type=image%2Fpng&disposition=inline/,
       @service.url(FIXTURE_KEY, expires_in: 5.minutes, disposition: :inline, filename: ActiveStorage::PostgreSQL::Filename.new("avatar.png"), content_type: "image/png"))
