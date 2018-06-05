b37274b72a0fa899a6c17544aee3319d02dad43a
Lachlan Sylvester <lachlan.sylvester@publicisfrontfoot.com.au>
2018-06-05 15:26:07 +1000

    delegate lo_ methods

--- lib/active_storage/postgresql/file.rb
+++ lib/active_storage/postgresql/file.rb
@@ -2,7 +2,7 @@ class ActiveStorage::PostgreSQL::File < ActiveRecord::Base
   self.table_name = "active_storage_postgresql_files"
 
   before_create do
-    self.oid ||= self.class.connection.raw_connection.lo_creat
+    self.oid ||= lo_creat
   end
 
   def self.open(key, &block)
@@ -12,41 +12,44 @@ class ActiveStorage::PostgreSQL::File < ActiveRecord::Base
   def open(*args)
     transaction do
       begin
-        @lo = self.class.connection.raw_connection.lo_open(oid, *args)
+        @lo = lo_open(oid, *args)
         yield(self)
       ensure
-        self.class.connection.raw_connection.lo_close(@lo) if @lo
+        lo_close(@lo) if @lo
       end
     end
   end
 
   def write(content)
-    self.class.connection.raw_connection.lo_write(@lo, content)
+    lo_write(@lo, content)
   end
 
   def read(bytes=size)
-    self.class.connection.raw_connection.lo_read(@lo, bytes)
+    lo_read(@lo, bytes)
   end
 
   def seek(position)
-    self.class.connection.raw_connection.lo_seek(@lo, position, 0)
+    lo_seek(@lo, position, 0)
   end
 
   def import(path)
-    self.oid = self.class.connection.raw_connection.lo_import(path)
+    self.oid = lo_import(path)
   end
 
   def size
-    current_position = self.class.connection.raw_connection.lo_tell(@lo)
-    self.class.connection.raw_connection.lo_seek(@lo, 0,2)
-    self.class.connection.raw_connection.lo_tell(@lo).tap do
-      self.class.connection.raw_connection.lo_seek(@lo, current_position,0)
+    current_position = lo_tell(@lo)
+    lo_seek(@lo, 0,2)
+    lo_tell(@lo).tap do
+      lo_seek(@lo, current_position,0)
     end
   end
 
   before_destroy do
     self.class.connection.raw_connection.lo_unlink(oid)
   end
+
+  delegate :lo_seek, :lo_tell, :lo_import, :lo_read, :lo_write, :lo_open,
+      :lo_unlink, :lo_close, :lo_creat, to: 'self.class.connection.raw_connection'
 
   scope :prefixed_with, -> prefix { where("key like ?", "#{prefix}%") }
 
