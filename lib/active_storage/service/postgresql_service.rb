5cdb37e71d48ab2dcea4b204a1c0df5fd182cf57
Lachlan Sylvester <lachlan.sylvester@3plearning.com>
2022-01-17 13:24:46 +1100

    add support for using public storage

--- lib/active_storage/service/postgresql_service.rb
+++ lib/active_storage/service/postgresql_service.rb
@@ -62,7 +62,21 @@ module ActiveStorage
       end
     end
 
-    def url(key, expires_in:, filename:, disposition:, content_type:)
+    def private_url(key, expires_in:, filename:, content_type:, disposition:, **)
+      generate_url(key, expires_in: expires_in, filename: filename, content_type: content_type, disposition: disposition)
+    end
+
+    def public_url(key, filename:, content_type: nil, disposition: :attachment, **)
+      generate_url(key, expires_in: nil, filename: filename, content_type: content_type, disposition: disposition)
+    end
+
+    def url(key, **options)
+      super
+    rescue NotImplementedError
+      private_url(key, **options)
+    end
+
+    def generate_url(key, expires_in:, filename:, disposition:, content_type:)
       instrument :url, key: key do |payload|
         content_disposition = content_disposition_with(type: disposition, filename: filename)
         verified_key_with_expiration = ActiveStorage.verifier.generate(
